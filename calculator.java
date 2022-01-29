37d152c095a4b752a46ec1744ab99c5b500102bb
sachin <sachinsingh1781998@gmail.com>
2022-01-29 11:08:29 +0530

    addition function

---
+++ calculator.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class calculator {
+
+    static void add(double a, double b) {
+        double r = a + b;
+        System.out.println("\n result=" + r);
+    }
+
+
+    public static void main(String[] args) {
+        double a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n enter first number:\n");
+
+        a = sc.nextDouble();
+        System.out.println("\n enter second number:\n");
+        b = sc.nextDouble();
+        while (true) {
+            System.out.println("1)add\n2)multiply\n3)subtract\n4)divide\n5)exit");
+            System.out.println("\nenter your choice=");
+            int c;
+            c = sc.nextInt();
+
+            switch (c) {
+                case 1:
+                    add(a, b);
+                    break;
+                // case 2:
+
+                //     multiply(a, b);
+                //     break;
+                // case 3:
+                //     subtract(a, b);
+                //     break;
+                // case 4:divide(a,b);break;
+
+                case 5:
+                    System.exit(0);
+                default:
+                    System.out.println("invalide input...");
+
+            }
+        }
+
+    }
+
+}
